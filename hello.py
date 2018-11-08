2e8476fa0b2d5c3bb4b29ef4af408342309c6d15
Gopikrishnan Chandrasekharan <gopikrishnanmc@users.noreply.github.com>
2018-11-08 13:19:34 -0600

    Made some minor changes to explain function. Still working

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
-
-
 class SolarSystem:
     def greet(self):
         document.getElementById('greet').innerHTML = 'Hello Gopi'
+
+    def explain(self):
+        document.getElementById('explain').innerHTML = 'Explain this'
+        console.log('Hello')
 
 
 solarSystem = SolarSystem()
